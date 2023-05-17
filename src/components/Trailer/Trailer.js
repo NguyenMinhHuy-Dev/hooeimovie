17268ae53d89c06bd5edf0624a6e250f9cbb09ec
minhhuy212 <nguyenminhhuy.0938745593@gmail.com>
2023-05-17 14:27:36 +0700

    update feature/detail/trailer

--- src/components/Trailer/Trailer.js
+++ src/components/Trailer/Trailer.js
@@ -32,6 +32,7 @@ export const Trailer = () => {
                             style={{
                                 minHeight: "100vh",
                                 pointerEvents: "none",
+                                userSelect: "none",
                             }}
                             width="100%" 
                             src={`https://www.youtube.com/embed/${trailer.key}?autoplay=1&mute=0&loop=1&color=black&modestbranding=1&playsnline=1&rel=0&controls=0&enablejsapi=1`}
