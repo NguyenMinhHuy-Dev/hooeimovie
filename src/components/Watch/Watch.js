ba41911b81789c8e7b6d6635521bef564118a721
minhhuy212 <nguyenminhhuy.0938745593@gmail.com>
2023-06-23 14:33:49 +0700

    watch movie iframe - similar movie

--- src/components/Watch/Watch.js
+++ src/components/Watch/Watch.js
@@ -1,34 +1,73 @@
 import { useParams } from 'react-router-dom';
 import './Watch.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Detail } from '../Detail/Detail';
+import { Similar } from '../Detail/Similar/Similar';
 
 export const Watch = () => {
     const { media_type, id, season_number, episode } = useParams(); 
 
-    return (
-        <div className='watch container'>
-            <div className='watch-player-episode'>
-                <div className='watch-left'>
-                    <iframe
-                        name="framez" 
-                        id="framez"
-                        width="100%"
-                        height={"100%"}
-                        src={`https://vidsrc.me/embed/${id}`}
-                        title="Movie player"
-                        // sandbox = "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" 
-                        frameBorder="0" 
-                        gesture="media" 
-                        allow="encrypted-media" 
-                        allowFullScreen
-                        
-                    />
-                </div>
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(false); 
 
-                <div className='watch-right'>
+    const scrollTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
 
+    useEffect(() => {
+        scrollTop();
+    },[])
+
+    useEffect(() => {
+        const getDetailMovie = async (media_type, id) => {
+            await fetch(`https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}`)
+            .then((res) => res.json())
+            .then((details) => {  
+                setData(details);
+                setLoading(false);  
+            })
+            .catch((err) => {
+                setLoading(false);
+            });
+        };  
+
+        setLoading(true);
+        scrollTop();
+        getDetailMovie(media_type, id); 
+    }, [id, media_type])
+    return ( 
+        <> 
+            <div className='watch container'>
+                <div className='watch-player-episode'>
+                    <div className={`watch-left ${media_type === 'movie' && 'full'}`}>
+                        <iframe
+                            name="framez" 
+                            id="framez"
+                            width="100%"
+                            height={"100%"}
+                            src={`https://vidsrc.me/embed/${id}`}
+                            title="Movie player"
+                            // sandbox = "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" 
+                            frameBorder="0" 
+                            gesture="media" 
+                            allow="encrypted-media" 
+                            allowFullScreen
+                            
+                        />
+                    </div>
+
+                    {media_type === 'tv' && 
+                        <div className='watch-right'>
+
+                        </div> 
+                    }
                 </div> 
-            </div>
-        </div>
+                
+                <Similar />
+            </div>   
+        </>
     );
 }
